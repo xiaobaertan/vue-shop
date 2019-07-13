555e5ff9b3588c3bcc122edc4ac6521afec8ce3b
Littlebaltan <1505993542@qq.com>
2019-07-13 19:03:43 +0800

    登录页面初步

---
+++ src/components/login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  width: 100%;
+  height: 100%;
+  .login_box {
+    background-color: #fff;
+    width: 450px;
+    height: 300px;
+    border-radius: 3px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    .avatar_box {
+        width: 130px;
+        height: 130px;
+        border-radius: 50%;
+        border: 1px solid #eee;
+        padding: 10px;
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        box-shadow: 0 0 10px #ddd;
+        background-color: #fff;
+        img {
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background: #eee;
+        }
+    }
+  }
+}
+</style>
+
+
