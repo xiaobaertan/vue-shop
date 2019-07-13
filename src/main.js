77fb597c418193921fdbb10caa2e8daf18e8d3fe
Littlebaltan <1505993542@qq.com>
2019-07-13 14:19:12 +0800

    我不知道对不对

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+import './assets/css/global.css'
 import './plugins/element.js'
+
 
 Vue.config.productionTip = false
 
